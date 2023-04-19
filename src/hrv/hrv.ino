5ab8fe531b70d7b94c0856ade2d8c24b798c0b7f
Tal Kogan <tal.kogan@campus.technion.ac.il>
2023-04-19 21:57:20 +0300

    Add RMSSD SDANN HRVI measurements and organize global variables

--- src/hrv/hrv.ino
+++ src/hrv/hrv.ino
@@ -1,3 +1,9 @@
+// Comment the following line if you want to run with real ECG signal
+#define SIMULATION
+
+///////////////////////////////////////////////////////////////////////////////
+////////////////////////////// 7 SEGMENT DISPLAY //////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 // Credit: https://github.com/DeanIsMe/SevSeg
 #include "SevSeg.h"
 
@@ -10,42 +16,74 @@ bool resistorsOnSegments = true;
 bool updateWithDelaysIn = true;
 byte hardwareConfig = COMMON_CATHODE;
 
-// Comment this line if you want to run with real ECG signal
-#define SIMULATION
-
-#define BPM_HIST_NUM_BINS (220)
-
-#ifdef SIMULATION
-#define RR_HIST_NUM_BINS (1200)
-int parsedSimulatedECGDataIndex = 0;
-int ecgDataIndex = 0;
-int ecgData[10000];
-bool simulationDone = false;
-#else   // !SIMULATION
-#define RR_HIST_NUM_BINS (1200)
-const int ecgPin = A0;
-#endif  // SIMULATION
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////// STATE ////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 
 const int buttonPin = 2;
 int buttonState = 0;
 
 bool monitoringDone = false;
+#ifdef SIMULATION
+bool simulationDone = false;
+#endif  // SIMULATION
 
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////// ECG /////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 bool alreadyPeaked = false;
-float beatsPerMinute = 0.0;
-int bpmi = 0;
 
 // TODO: Figure out these values for real ECG Signal
 int ecgOffset = 0;
 int lowerThreshold = -400;
 int upperThreshold = 400;
 
+#ifdef SIMULATION
+int parsedSimulatedECGDataIndex = 0;
+int ecgDataIndex = 0;
+int ecgData[10000];
+#else   // !SIMULATION
+const int ecgPin = A0;
+#endif  // SIMULATION
+
+///////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////// RR /////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+#define BPM_HIST_NUM_BINS (220)
+#define RR_HIST_NUM_BINS (1200)
+
 unsigned int rrIntervalsHistogram[RR_HIST_NUM_BINS] = {};
 unsigned int bpmHistogram[BPM_HIST_NUM_BINS + 1] = {};
 
 unsigned long firstPeakTime = 0;
+unsigned long prevrrInterval = 0;
 unsigned long rrInterval = 0;
 unsigned long secondPeakTime = 0;
+
+float beatsPerMinute = 0.0;
+int bpmi = 0;
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////// RMSSD ////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+unsigned long rmssdSum = 0;
+
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////// SDANN ////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+#define SDANN_INTERVAL (5 * 60 * 1000)  // 5min = 5 * 60[s] * 1000[ms]
+#define ANN_ARRAY_SIZE ((24 * 60 * 60 * 1000) / SDANN_INTERVAL)
+double ann[ANN_ARRAY_SIZE];
+unsigned int annIndex = 0;
+unsigned int annSum = 0;
+unsigned int annCount = 0;
+unsigned long annTime = 0;
+
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// MEASUREMENTS /////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+double rmssd = 0.0;
+double sdann = 0.0;
+double HRVI = 0.0;
 
 void dumpRRIntervalHistogram() {
     // Dump RR histogram
@@ -62,6 +100,65 @@ void dumpBPMHistogram() {
         SerialUSB.println(bpmHistogram[i]);
     }
 
+    SerialUSB.println("Done");
+}
+
+void dumpRMSSD() {
+    if (rmssd == 0.0) {
+        // Calculate the RMS
+        int numRRDifferences = 0;
+        for (int i = 0; i < RR_HIST_NUM_BINS; ++i) {
+            numRRDifferences += rrIntervalsHistogram[i];
+        }
+
+        // We want number of differences not number of intervals, so minus 1
+        numRRDifferences--;
+        rmssd = sqrt(rmssdSum / numRRDifferences);
+    }
+
+    SerialUSB.println(rmssd);
+    SerialUSB.println("Done");
+}
+
+void dumpSDANN() {
+    if (sdann == 0.0 && annIndex != 0) {
+        // Calculate the mean
+        unsigned long sum = 0;
+        for (int i = 0; i < annIndex; ++i) {
+            sum += ann[i];
+        }
+
+        double mean = sum / annIndex;
+
+        // Calculate the standard deviation
+        for (int i = 0; i < annIndex; ++i) {
+            sdann += pow(ann[i] - mean, 2);
+        }
+
+        sdann = sqrt(sdann / annIndex);
+    }
+
+    SerialUSB.println(sdann);
+    SerialUSB.println("Done");
+}
+
+void dumpHTI() {
+    if (hti == 0.0) {
+        unsigned long rrIntervalHistogramDensity = 0;
+        unsigned long rrIntervalHistogramHeight = 0;
+
+        for (int i = 0; i < RR_HIST_NUM_BINS; ++i) {
+            // There are rrIntervalsHistogram[i] instances of the RR interval i
+            rrIntervalHistogramDensity += rrIntervalsHistogram[i];
+            if (rrIntervalsHistogram[i] > rrIntervalHistogramHeight) {
+                rrIntervalHistogramHeight = rrIntervalsHistogram[i];
+            }
+        }
+
+        hti = rrIntervalHistogramDensity / rrIntervalHistogramHeight;
+    }
+
+    SerialUSB.println(hti);
     SerialUSB.println("Done");
 }
 
@@ -96,6 +193,30 @@ void setup() {
 #endif  // SIMULATION
 
     digitalWrite(LED_BUILTIN, LOW);
+    annTime = millis();
+}
+
+void dispatchCommand() {
+    int action = SerialUSB.parseInt();
+    switch (action) {
+        case 1:
+            dumpRRIntervalHistogram();
+            break;
+        case 2:
+            dumpBPMHistogram();
+            break;
+        case 3:
+            dumpRMSSD();
+            break;
+        case 4:
+            dumpSDANN();
+            break;
+        case 5:
+            dumpHRVI();
+            break;
+        default:
+            break;
+    }
 }
 
 void loop() {
@@ -110,17 +231,7 @@ void loop() {
 
     if (monitoringDone) {
         if (SerialUSB.available() > 0) {
-            int action = SerialUSB.parseInt();
-            switch (action) {
-                case 1:
-                    dumpRRIntervalHistogram();
-                    break;
-                case 2:
-                    dumpBPMHistogram();
-                    break;
-                default:
-                    break;
-            }
+            dispatchCommand();
         }
 
         delay(10);
@@ -165,6 +276,20 @@ void loop() {
             secondPeakTime = millis();
             rrInterval = secondPeakTime - firstPeakTime;
             rrIntervalsHistogram[rrInterval]++;
+
+            // If at least SDANN_INTERVAL milliseconds passed since annTime
+            if (secondPeakTime - annTime >= SDANN_INTERVAL) {
+                // Save the average NN over the current time period
+                ann[annIndex++] = annSum / annCount;
+
+                // Reset parameters
+                annTime = millis();
+                annSum = 0;
+                annCount = 0;
+            }
+
+            annSum += rrInterval;
+            annCount++;
 
             // Calculate the beats per minute, rrInterval is measured in
             // milliseconds so we must multiply by 1000
